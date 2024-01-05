3217794fe5bed0a4d6a66167ca5fee2b5f40f3a5
Bruno <bruno_scanto@hotmail.com>
2024-01-05 16:06:14 -0300

    Mcroservice

---
+++ Microservice_java/user/src/main/java/com/ms/user/dtos/UserRecordDto.java
@@ -0,0 +1,10 @@
+package com.ms.user.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record UserRecordDto(@NotBlank String name,
+                            @NotBlank @Email String email){
+
+
+}
---
+++ Microservice_java/user/src/main/java/com/ms/user/services/UserService.java
@@ -0,0 +1,23 @@
+package com.ms.user.services;
+
+import com.ms.user.models.UserModel;
+import com.ms.user.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+
+    final
+    UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public UserModel save(UserModel userModel){
+        return userRepository.save(userModel);
+    }
+
+}
